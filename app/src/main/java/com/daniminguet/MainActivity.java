8336b7e025133b0795236ffc5dec36d0538b6b20
dminguet <daminto2001@gmail.com>
2023-03-16 18:18:04 +0100

    Implementación menú

--- app/src/main/java/com/daniminguet/MainActivity.java
+++ app/src/main/java/com/daniminguet/MainActivity.java
@@ -1,8 +1,14 @@
 package com.daniminguet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.daniminguet.fragments.FragmentPerfil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.iPerfil) {
+            FragmentManager manager = getSupportFragmentManager();
+            manager.beginTransaction()
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .replace(R.id.frgPrincipal, FragmentPerfil.class, null)
+                    .commit();
+            return true;
+        }
+
+        if (id == R.id.iCerrarSesion) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
