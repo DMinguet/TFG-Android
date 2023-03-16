4e7fca43b5530a9715b4028f5d54dfef5e105728
dminguet <daminto2001@gmail.com>
2023-03-16 18:14:14 +0100

    Logo y varios layouts

--- app/src/main/java/com/daniminguet/LoginActivity.java
+++ app/src/main/java/com/daniminguet/LoginActivity.java
@@ -1,15 +1,12 @@
 package com.daniminguet;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.daniminguet.models.Usuario;
 import com.daniminguet.rest.RestClient;
 
 import java.security.NoSuchAlgorithmException;
---
+++ app/src/main/java/com/daniminguet/fragments/FragmentPrincipal.java
@@ -0,0 +1,29 @@
+package com.daniminguet.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.daniminguet.R;
+
+public class FragmentPrincipal extends Fragment {
+
+
+    public FragmentPrincipal() {
+        super(R.layout.principal);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+}
